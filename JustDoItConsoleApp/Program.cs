b1656d6564e0472f6eef7891e04499acea539dab
JuanGut01 <juan.gutierrez@edu.tbz.ch>
2021-11-02 16:34:58 +0100

    Add project files.

---
+++ JustDoItConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JustDoItConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
