52268e0bbc90f717dc6198c2ab71586b6dd93a16
Gani Nurceski <ganinurceski@gmail.com>
2021-11-02 16:49:33 +0100

    first commit

---
+++ JustDoItConsoleApp/task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustDoItConsoleApp
+{
+    class Task
+    {
+        public string title;
+        public string priority;
+        public DateTime deadline;
+        public int taskId;
+        public bool finished = false;
+    }
+}
---
+++ JustDoItConsoleApp/taskservice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustDoItConsoleApp
+{
+    class Taskservice
+    {
+        public List<Task> tasks = new List<Task>();
+        public void add(string title, string priority, DateTime deadline)
+        {
+            var task = new Task();
+            task.title = title;
+            task.priority = priority;
+            task.deadline = deadline;
+            task.taskId = 1;
+            tasks.Add(task);
+
+        }
+        public void edit(string title, string priority, DateTime deadline, int taskId)
+        {
+            foreach (Task t in tasks)
+            {
+                if (t.taskId == taskId)
+                {
+                    t.title = title;
+                    t.priority = priority;
+                    t.deadline = deadline;
+
+
+                }
+            }
+        }
+        public void complete()
+        {
+
+        }
+        public void delete()
+        {
+
+        }
+    }
+}
