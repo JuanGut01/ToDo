36a6b951b465a88f95a73645589fae47eb96ca3f
JuanGut01 <juan.gutierrez@edu.tbz.ch>
2021-11-02 16:47:32 +0100

    Added UI

---
+++ JustDoItConsoleApp/UI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustDoItConsoleApp
+{
+    class UI
+    {
+
+        private string Menu = @"
+          MENU
+ -----------------------
+¦   1.  Add Task        ¦
+¦   2.  Remove Task     ¦
+¦   3.  Exit            ¦
+ -----------------------
+
+";
+
+        public void StartUi()
+        {
+            Console.Clear();
+            ShowMenu();
+            ReadInput();
+        }
+        public void ReadInput()
+        {
+            int Input;
+
+            try
+            {
+                Input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Letters and special characters are not permitted. Please choose one of the numbers shown above.");
+                Input = Convert.ToInt32(Console.ReadLine());
+            }
+
+            switch (Input)
+            {
+                case 1:
+                    Console.WriteLine("\nAdd task clicked");
+                    //Add.Task()
+                    ReadInput();
+                    break;
+                case 2:
+                    Console.WriteLine("\nRemove task clicked");
+                    //Remove.Task()
+                    ReadInput();
+                    break;
+                case 3:
+                    break;
+                default:
+                    Console.WriteLine("\nThat option is incorrect, please try again");
+                    ReadInput();
+                    break;
+            }
+        }
+        public void ShowMenu()
+        {
+            Console.WriteLine(Menu);
+        }
+
+    }
+}
