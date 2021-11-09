e79870e31de207b1e0daafbc082f236ad8edf3d2
naik3x <86108070+naik3x@users.noreply.github.com>
2021-11-09 22:03:08 +0100

    Update Program.cs
    fixed bug

--- JustDoItConsoleApp/Program.cs
+++ JustDoItConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Http;
 
@@ -6,19 +6,10 @@ namespace JustDoItConsoleApp
 {
     class Program
     {
-        private const string URL = "https://worldtimeapi.org/api/timezone/europe/zurich";
         static void Main(string[] args)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
             UI UI = new UI();
-            //TimeAPI();
-
             UI.StartUi();
         }
-        //private static string ShowApi()
-        //{
-            //Console.WriteLine("$timestamp");
-        //}
     }
 }
