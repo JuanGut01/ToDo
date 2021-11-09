4dd60cb6b69a4a545750f163c9338e0b79797f67
naik3x <ganinurceski@gmail.com>
2021-11-09 22:00:15 +0100

    Time API added

--- JustDoItConsoleApp/Program.cs
+++ JustDoItConsoleApp/Program.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace JustDoItConsoleApp
 {
     class Program
     {
+        private const string URL = "https://worldtimeapi.org/api/timezone/europe/zurich";
         static void Main(string[] args)
         {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
             UI UI = new UI();
+            //TimeAPI();
 
             UI.StartUi();
         }
+        //private static string ShowApi()
+        //{
+            //Console.WriteLine("$timestamp");
+        //}
     }
 }
---
+++ JustDoItConsoleApp/TimeAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace JustDoItConsoleApp
+{
+    public class TimeAPI
+    {
+
+
+        private HttpClient client = new HttpClient();
+
+        private Dictionary<string, string> info = new Dictionary<string, string>();
+
+
+        public async void getInfo()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://worldtimeapi.org/api/timezone/europe/zurich");
+    
+        HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            string body = await response.Content.ReadAsStringAsync();
+
+
+            info = JsonConvert.DeserializeObject<Dictionary<string, string>>(body);
+            showInfo();
+
+        }
+
+        private void showInfo()
+        {
+            Console.WriteLine("Today is the " + info["datetime"] + " welcome in " + info["timezone"]);
+        }
+
+    }
+}
